baaaa01eb41e28a2bca3550288d089441b59b77b
JosukeSugiura <sugiura@irvinesystems.co.jp>
2022-07-08 18:55:59 +0900

    writing insert method

--- linked_list_class.py
+++ linked_list_class.py
@@ -7,7 +7,20 @@ class LinkedList:
     def __init__(self,value):
         self.head = Node(value)
     
-    def show_list(self):
+    def getListLength(self):
+        self.list_length = 0
+        all_next = self.head
+        while all_next.next != None:
+            list_length += 1
+            all_next = all_next.next
+        
+        return list_length
+    
+'''    def judgeListPlace(self,place):
+        
+        if place'''
+
+    def showList(self):
         all_next = self.head
         while all_next.next != None:
             print(all_next.value)
@@ -15,14 +28,19 @@ class LinkedList:
         print(all_next.value)
         #print(self.head.value)
 
-    def insert(self,value):
+    def push(self,value):
         all_next = self.head
         while all_next.next != None:
             all_next = all_next.next
         all_next.next = Node(value)
 #        self.head.next = Node(value)
 
+    def insert(self,place,value):
+        all_next = self.head
+        while
+
+
 link = LinkedList(10)
-link.insert(5)
-link.insert(12)
-link.show_list()
+link.push(5)
+link.push(12)
+link.showList()
