d2f64a5971b376e1eb2bc89a7f0e4f9ce1d3a0c7
JosukeSugiura <sugiura@irvinesystems.co.jp>
2022-07-13 17:31:51 +0900

    fixed insert method

--- linked_list_class.py
+++ linked_list_class.py
@@ -13,34 +13,63 @@ class LinkedList:
         while all_next.next != None:
             list_length += 1
             all_next = all_next.next
-        
+
         return list_length
     
-'''    def judgeListPlace(self,place):
-        
-        if place'''
+#    def judgeListPlace(self,place):
+#        if place 
 
     def showList(self):
+        all_next = self.head
+        all_values = []
+        while all_next.next != None:
+            all_values.append(all_next.value)
+            all_next = all_next.next
+        return all_values
+        #print(self.head.value) 
+
+    def pushNode(self,value):
+        all_next = self.head
+        while all_next.next != None:
+            all_next = all_next.next
+        all_next.next = Node(value)
+
+    def insertNode(self,place,value):
+        all_next = self.head
+        node_inserted = Node(value)
+        for i in range(place-1):
+            all_next = all_next.next
+        node_inserted.next = all_next.next
+        all_next.next = node_inserted
+    
+    def deleteNode(self,place):
+        all_next = self.head
+        for i in range(place-1):
+            all_next = all_next.next
+        
+
+'''    def showList(self):
         all_next = self.head
         while all_next.next != None:
             print(all_next.value)
             all_next = all_next.next
         print(all_next.value)
-        #print(self.head.value)
+        #print(self.head.value) 
 
     def push(self,value):
         all_next = self.head
         while all_next.next != None:
             all_next = all_next.next
         all_next.next = Node(value)
+                                        '''
 #        self.head.next = Node(value)
 
-    def insert(self,place,value):
-        all_next = self.head
-        while
 
 
-link = LinkedList(10)
-link.push(5)
-link.push(12)
-link.showList()
+link = LinkedList(0)
+for i in range(4):
+    link.pushNode(i+1)
+print(link.showList())
+print('---')
+link.insertNode(0,1.5)
+print(link.showList())
