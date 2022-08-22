e8f66a8b8f973bc7843bdca3948c19f33fa03630
JosukeSugiura <sugiura@irvinesystems.co.jp>
2022-08-22 18:48:25 +0900

    writing mytree

--- mytree.py
+++ mytree.py
@@ -1,12 +1,40 @@
-class Noede:
-    def __init__(self, int,value):
-        self.int = int
+class Node:
+    def __init__(self, key,value):
+        self.key = key
         self.value = value
         self.left = None
         self.right = None
 
 class Tree:
     def __init__(self):
-        self.head = None
+        self.root = None
+    
+    def sarchNode(self,key):
+        current = 
+        targetKey = current.key
+        if key > current.key:
+            current = current.right
+            sarchNode(current.key)
+        elif key < current.key:
+            current = current.left
+            sarchNode(current.key)
+        else:
+            return current.value
+    
+    def addNode(self,key):
+        current = self.root
+        #targetKey = current.key
+        if  key == current.key :
+            pass
+        elif key > current.key:
+            if current.right == None:
+                current.right = Node(key)
+            else:
+                current = current.right
+        else: 
+            if current.left == None:
+                current.left = Node(key)
+            else:
+                addNode(key)
 
-    def add
+    def deleteNoede(self,key):
