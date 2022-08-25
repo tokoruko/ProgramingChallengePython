d829e7751b023c960e957b6dddf21958307175c4
JosukeSugiura <sugiura@irvinesystems.co.jp>
2022-08-25 19:00:09 +0900

    commented

--- mytree.py
+++ mytree.py
@@ -1,40 +1,87 @@
-class Node:
-    def __init__(self, key,value):
-        self.key = key
-        self.value = value
-        self.left = None
-        self.right = None
+# class Node:
+#     def __init__(self, key):#,value):
+#         self.key = key
+#         #self.value = value
+#         self.left = None
+#         self.right = None
 
 class Tree:
-    def __init__(self):
-        self.root = None
+    # def __init__(self):
+    #     self.root = None
+
+    def __init__(self,key):
+        self.key = key
+        #self.value = value
+        self.left = None
+        self.right = None
     
     def sarchNode(self,key):
-        current = 
-        targetKey = current.key
-        if key > current.key:
-            current = current.right
-            sarchNode(current.key)
-        elif key < current.key:
-            current = current.left
-            sarchNode(current.key)
+        if key > self.key:
+            current = self.right
+            self.sarchNode(current.key)
+        elif key < self.key:
+            current = self.left
+            self.sarchNode(current.key)
         else:
-            return current.value
+            return self.key
     
     def addNode(self,key):
-        current = self.root
+        #current = self.key
         #targetKey = current.key
-        if  key == current.key :
+        if  key == self.key :
             pass
-        elif key > current.key:
-            if current.right == None:
-                current.right = Node(key)
+        elif key > self.key:
+            if self.right == None:
+                self.right = Tree(key)
             else:
-                current = current.right
+                current = self.right
+                addNode(current.key)
         else: 
-            if current.left == None:
-                current.left = Node(key)
+            if self.left == None:
+                self.left = Tree(key)
             else:
-                addNode(key)
+                current = self.left
+                addNode(current.key)
 
-    def deleteNoede(self,key):
+
+    # def sarchNode(self,key):
+    #     current = self.root
+    #     #targetKey = current.key
+
+    #     while current.key != key:
+    #         current.left.key 
+    #         current.right.key
+    #     return current.key
+
+        # if key > current.key:
+        #     current = current.right
+        #     sarchNode(current.key)
+        # elif key < current.key:
+        #     current = current.left
+        #     sarchNode(current.key)
+        # else:
+        #     return current.value
+    
+    # def addNode(self,key):
+    #     current = self.root
+    #     #targetKey = current.key
+    #     if  key == current.key :
+    #         pass
+    #     elif key > current.key:
+    #         if current.right == None:
+    #             current.right = Node(key)
+    #         else:
+    #             current = current.right
+    #     else: 
+    #         if current.left == None:
+    #             current.left = Node(key)
+    #         else:
+    #             current = current.left
+
+    #def deleteNoede(self,key):
+
+print("when 0")
+tree = Tree(50)
+tree.addNode(5)
+tree.sarchNode(5)
+
